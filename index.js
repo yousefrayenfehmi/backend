b3d4b04b8d3fb44191de10bdc318ac3a5abf946d
youssefrayenfehmi <yousseffehmi99@gmail.com>
2024-06-14 11:51:42 +0100

    first

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express=require('express');
-let dotenv = require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
 const formation=require('./controlleur/formationControlleur')
 const router=require("./route/route")
 const app=express();
 const cors=require("cors")
-port=process.env.Port||3000;
+port=3000;
 app.use(cors())
 app.use(router);
 
